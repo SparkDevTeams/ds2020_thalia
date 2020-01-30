fc0711371ebb58a61be9865f351ba8591ad1945f
tmaidique <45432013+tmaidique@users.noreply.github.com>
2020-01-30 17:00:26 -0500

    original files

---
+++ client/build/precache-manifest.947da3e3737fd490ac67ef4f66ff5991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84948d1d1ccd296158bb1f9e1b7ed1bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41b7b81d3234e788305",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5b59e83441d21a52750d",
+    "url": "/static/css/main.fd4607c5.chunk.css"
+  },
+  {
+    "revision": "c41b7b81d3234e788305",
+    "url": "/static/js/2.8d6739a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8d6739a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b59e83441d21a52750d",
+    "url": "/static/js/main.b480fee9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "c9f3de04c4aff31524ce87aa720b82d5",
+    "url": "/static/media/crystall_ball.c9f3de04.png"
+  }
+]);
