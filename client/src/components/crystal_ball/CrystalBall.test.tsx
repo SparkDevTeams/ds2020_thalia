13353a3a955625e94a5a32869f2203d5e516e33a
tmaidique <45432013+tmaidique@users.noreply.github.com>
2020-02-06 16:56:49 -0500

    updated for front end testing

--- client/src/components/crystal_ball/CrystalBall.test.tsx
+++ client/src/components/crystal_ball/CrystalBall.test.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import CrystalBall from './CrystalBall';
+import React from "react";
+import { render, fireEvent, act, waitForElement } from "@testing-library/react";
+import CrystalBall from "./CrystalBall";
+import { unmountComponentAtNode } from "react-dom";
+import axios from "axios";
 
-test('renders CrystalBall', () => {
+jest.mock("axios");
+
+let container: any = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test("renders CrystalBall", () => {
+  let linkElement: any = null;
+
+  act(() => {
     const { getByText } = render(<CrystalBall />);
-    const linkElement = getByText(/What is your future/i);
-    expect(linkElement).toBeInTheDocument();
+    linkElement = getByText(/What is your future/i);
+  });
+
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders message from backend", async () => {
+  // Setup
+  axios.get.mockResolvedValue({ data: "message!" });
+  act(() => {
+    const { getByText } = render(<CrystalBall />);
+    fireEvent.click(getByText(/Get My Fortune Told/i));
+  });
+
+  // Get results
+  const msgElement: any = await waitForElement(() =>
+    document.getElementById("msg")
+  );
+  const text: string = msgElement.innerHTML.valueOf();
+
+  // Assert
+  expect(msgElement).toBeInTheDocument();
+  expect(text).toContain("message!");
 });
