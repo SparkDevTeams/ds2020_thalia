b10f18d2cea0d39c3a718a4265545cd7c7ac2792
tmaidique <45432013+tmaidique@users.noreply.github.com>
2020-02-06 18:27:23 -0500

    to use the queryByTestId

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 import { unmountComponentAtNode } from "react-dom";
+import '@testing-library/jest-dom'
 
 let container: any = null;
 beforeEach(() => {
